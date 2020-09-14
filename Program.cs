a8e176df1cc1eb6c6b9e8a4e6c2f1834bb0b6cb4
Andre Pells <Apells1@my.wctc.edu>
2020-09-13 23:06:17 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
-
+using NLog.Web;
 namespace SleepData
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program started");
             string[] daysOfWeek = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"  };
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
@@ -60,13 +64,15 @@ namespace SleepData
                   string daysOfWeek2 = string.Join(" ", daysOfWeek);
                 //   Console.WriteLine(parsedLineOfCode[0]);
               DateTime test = Convert.ToDateTime(parsedLineOfCode[0]);
-                Console.WriteLine($"Week of  {test:MMM}, {parsedLineOfCode[0]}");
-                Console.WriteLine("   "+daysOfWeek2);
-                Console.WriteLine("   -- -- -- -- -- -- --");
-                   Console.WriteLine(decoded+ "\n");
+                Console.WriteLine($"Week of {test:MMM}, {parsedLineOfCode[0]}");
+                Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}"); // {"Tot",4}{"Avg",4}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}"); // {"---",4}{"---",4}");
+                   Console.WriteLine($"{totalParsed[0],3}{totalParsed[1],3}{totalParsed[2],3}{totalParsed[3],3}{totalParsed[4],3}{totalParsed[5],3}{totalParsed[6],3}"); 
+                     Console.WriteLine();// {hours.Sum(),4}{Math.Round(hours.Sum() / 7.0, 1),4:n1}");
 
                   }
             }
+              logger.Info("Program ended");
         }
     }
 }
